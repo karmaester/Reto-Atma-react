ae103ca09b96637ee5fd80c33ce6543aa994d982
Khristian Rojas <khristianrojas15@gmail.com>
2021-06-01 00:48:31 -0400

    Add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      Landing page
+    <div className={classes.root}>
+      <CssBaseline />
     </div>
   );
 }
